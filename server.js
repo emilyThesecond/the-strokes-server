34b7203cc9fb86b928ca813315157ee85b5754b6
emilyTheseond <esegundo02@gmail.com>
2024-02-07 09:06:40 -0600

    Connect Database

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ require('dotenv').config()
 
 require('./config/db.connection')
 
+const { PORT } = process.env
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
