5291d6af286a3e50e17246566ced1d72fd88ff66
emilyTheseond <esegundo02@gmail.com>
2024-02-12 10:29:37 -0600

    update post controller

--- controllers/sets.js
+++ controllers/sets.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const Shows	= require('../models/Show')
+const Show	= require('../models/Show')
 
 
 const create = async(req,res,next)	=> {
-	const show = await Show.findById(req.params.id);
-
-	show.set.push(req.body);
+	const show = await Show.findById(req.params.id)
+	show.set.push(req.body)
 	try {
-	  await show.save();
+	  await show.save()
 	} catch (err) {
-	  console.log(err);
+	  console.log(err)
 	}
-	res.redirect(`/shows/${show._id}`);
+	res.redirect(`/shows/${show._id}`)
   }
+
 module.exports = {
 	create,
 }
